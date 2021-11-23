0c35c7f53b6d4171468404d19a50d62c29d81532
yeonil <ahnyi527@gmail.com>
2021-11-23 16:53:15 +0900

    djangogram_5_2_FormCss

--- djangogram/djangogram/users/forms.py
+++ djangogram/djangogram/users/forms.py
@@ -12,42 +12,44 @@ class UserChangeForm(forms.UserChangeForm):
 
 
 class UserCreationForm(forms.UserCreationForm):
-    
+
     error_message = forms.UserCreationForm.error_messages.update(
         {"duplicate_username": _("This username has already been taken.")}
     )
-    
+
     class Meta(forms.UserCreationForm.Meta):
         model = User
 
     def clean_username(self):
-        username = self.cleand_date["username"]
-        
+        username = self.cleaned_data["username"]
+
         try:
             User.objects.get(username=username)
         except User.DoesNotExist:
             return username
-        
-        raise ValidationError(self.error_message["duplicate_username"])
-    
+
+        raise ValidationError(self.error_messages["duplicate_username"])
+
 class SignUpForm(django_forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'name', 'username', 'password']
-        
-        labels = {
-            'email' : '이메일 주소',
-            'name' : '성명',
-            'username' : '사용자 이름',
-            'password' : '비밀번호'
-        }
-        
+
+        # labels = {
+        #     'email': '이메일 주소',
+        #     'name': '성명',
+        #     'username': '사용자 이름',
+        #     'password': '비밀번호'
+        # }
+
         widgets = {
-            'password' : django_forms.PasswordInput(),
+            'email': django_forms.TextInput(attrs={'placeholder': '이메일 주소'}),
+            'name': django_forms.TextInput(attrs={'placeholder': '성명'}),
+            'username': django_forms.TextInput(attrs={'placeholder': '사용자 이름'}),
+            'password': django_forms.PasswordInput(attrs={'placeholder': '비밀번호'}),
         }
-        
+
     def save(self, commit=True):
-        # Save the provided password in hashed format
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password"])
         if commit:
